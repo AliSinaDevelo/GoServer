309d8b0c7f292be4fa298b57071b5c118dec7d3c
AliSina Karimi <100498538+AliSinaDevelo@users.noreply.github.com>
2023-12-08 23:28:53 +0100

    initial handlers and exceptiond created in main.go

--- main.go
+++ main.go
@@ -7,5 +7,14 @@ import (
 )
 
 func main() {
-	
+	// check on the static directory
+	file_server := http.FileServer(http.Dir("./static"))
+	http.Handle("/", file_server)
+	http.HandleFunc("/form", form_handler) 
+	http.HandleFunc("/hello", hello_handler)
+
+	fmt.Printf("Server starting at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
